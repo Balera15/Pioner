5e95732cac1978c83f64cd98cefcafddefd3dc6a
Balera15 <humanpochta@gmail.com>
2024-12-02 10:24:55 +0300

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,4 @@
+
 const cardsData = [
   {
     image: "./img/kamaz1 1.png",
@@ -23,61 +24,51 @@ const maxCards = 50;
 function createCard(cardData) {
   const cardElement = document.createElement("div");
   cardElement.classList.add("card");
-
   const imgElement = document.createElement("img");
   imgElement.src = cardData.image;
   imgElement.alt = cardData.alt;
   imgElement.classList.add("card__image");
-
+  //клик по ихображению
+  imgElement.addEventListener("click", () => {
+    openModal(cardData.image); });
   const cardInfo = document.createElement("div");
   cardInfo.classList.add("card__info");
-
   const cardTitle = document.createElement("p");
   cardTitle.classList.add("card__title");
   cardTitle.textContent = cardData.title;
-
   const likeButton = document.createElement("button");
   likeButton.classList.add("card__like-button");
   likeButton.textContent = "LIKE";
-
   cardInfo.appendChild(cardTitle);
   cardInfo.appendChild(likeButton);
   cardElement.appendChild(imgElement);
   cardElement.appendChild(cardInfo);
-
   return cardElement;
 }
 //скелет
 function createSkeleton() {
   const skeletonElement = document.createElement("div");
   skeletonElement.classList.add("card__skeleton");
-
   const imageSkeleton = document.createElement("div");
   imageSkeleton.classList.add("card__image-skeleton");
-
   const loaderGif = document.createElement("img");
   loaderGif.src = "./img/preloader.gif";
   loaderGif.alt = "Загрузка...";
   loaderGif.classList.add("card__loader");
   imageSkeleton.appendChild(loaderGif);
-
   const titleSkeleton = document.createElement("div");
   titleSkeleton.classList.add("card__title-skeleton");
-
   const buttonSkeleton = document.createElement("div");
   buttonSkeleton.classList.add("card__like-button-skeleton");
-
   skeletonElement.appendChild(imageSkeleton);
   skeletonElement.appendChild(titleSkeleton);
   skeletonElement.appendChild(buttonSkeleton);
-
   return skeletonElement;
 }
 //addskelet
 function addCard(cardData, delay) {
   const skeleton = createSkeleton();
   cardContainer.appendChild(skeleton);
-
   //смена
   setTimeout(() => {
     const cardElement = createCard(cardData);
@@ -116,3 +107,22 @@ window.addEventListener('scroll', () => {
   }
 });
 loadCards();
+//модальное окно с картинкой
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modal-image");
+const modalClose = document.getElementById("modal-close");
+//Открыть модальное окно с картинкой
+function openModal(imageSrc) {
+  modal.style.display = "flex";
+  modalImage.src = imageSrc;
+}
+//Закрыть
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+//Клик вне картинки, чтобы закрыть
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
